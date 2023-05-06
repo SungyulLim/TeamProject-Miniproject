aa003e3ffff53743cc4a1a8b2385552f451f461f
sumina729 <114402652+sumina729@users.noreply.github.com>
2023-05-06 13:39:16 +0900

    manager update

--- manager.c
+++ manager.c
@@ -10,22 +10,6 @@
 void manager_main(){
     printf("관리자용 서브메인 입니다\n");
 
-    //test
-    printf("-----------------------LIST-------------------\n");
-    for(int i=0; i<count; i++){
-        
-        printf("%s %s %s %s %d호 %d인실 %d점 %c\n", students[i]->name, students[i]->s_ID, students[i]->f_m, students[i]->Dormitory_name, students[i]->room_num, students[i]->total_num_room, students[i]->demerit, students[i]->Y_N); 
-        
-    }
-    printf("------------------------------------------------\n\n");
-
-    for(int i=0; i<count; i++){
-        if(students[i] ->Y_N == 'N'){
-            students[i] ->room_num = 101+i;
-            students[i] ->Y_N = 'Y';
-            printf("%s(%s)님을 %d호에 방배정 했습니다.\n", students[i] ->name, students[i] ->s_ID, students[i] ->room_num);
-        }
-    }
 
     while(1){
         int menu;
@@ -34,7 +18,8 @@ void manager_main(){
         printf("1. 기숙사 신청자 방 배치\n");
         printf("2. 기숙사 리스트 보기\n");
         printf("3. 벌점 부과하기\n");
-        printf("5. 리스트 저장\n");
+        printf("4. 리스트 저장\n");
+        printf("5. 리스트 불러오기\n");
         printf("6. 메인 화면으로\n");
         printf("----------------------\n");
 
@@ -43,19 +28,19 @@ void manager_main(){
 
         if (menu == 6) break;
         if (menu == 1){
-            
+            room_numbering();
         }
         else if (menu == 2){
-            
+            dormitory_list();
         }
         else if (menu == 3){
-            
+            penalty_points();
         }
         else if (menu == 4){
-            
+            save_list();
         }
         else if (menu ==5){
-            
+            recall_list();
         }
         else{
             printf("\n!!입력오류!!\n");
@@ -74,3 +59,54 @@ void manager_main(){
 }
 
 //여기 밑에 각 기능을 위한 함수 구현. 헤더파일에 헤더부분 꼭 선언 해야 오류 안남.
+
+//1. 기숙사 신청자 방 배치
+void room_numbering(){
+    int num;
+    printf("\n-----------------------기숙사 신청자 리스트-------------------\n");
+    printf("번호 이름    학번   성별 생활관  호실 인실  벌점 방배정여부\n");
+    for(int i=0; i<count; i++){
+        if(students[i] ->Y_N == 'N'){
+            printf("%2d. %-4s %s  %s  %-4s %3d호 %d인실 %2d점  %3c\n",i+1 , students[i]->name, students[i]->s_ID, students[i]->f_m, students[i]->Dormitory_name, students[i]->room_num, students[i]->total_num_room, students[i]->demerit, students[i]->Y_N); 
+        }
+    }
+    printf("----------------------------------------------------------\n\n");
+    printf("방 배정 할 학생의 번호를 입력하세요 > ");
+    scanf("%d", &num);
+    num--; //배열이라 -1 해줘여함.
+
+    printf("%s(%s)학우님을 배정학 방 호수를 입력하세요 > ", students[num]->name, students[num]->s_ID);
+    scanf("%d", &students[num] ->room_num);
+    students[num] ->Y_N = 'Y';
+
+    printf("%s(%s)학우님을 %d호에 방배정 했습니다.\n", students[num] ->name, students[num] ->s_ID, students[num] ->room_num);
+}
+
+//2. 기숙사 리스트 보기
+void dormitory_list(){
+    printf("\n----------------------------전체 리스트------------------------\n");
+    printf("번호 이름    학번   성별 생활관  호실 인실  벌점 방배정여부\n");
+    for(int i=0; i<count; i++){
+        
+        printf("%2d. %-4s %s  %s  %-4s %3d호 %d인실 %2d점  %3c\n",i+1 , students[i]->name, students[i]->s_ID, students[i]->f_m, students[i]->Dormitory_name, students[i]->room_num, students[i]->total_num_room, students[i]->demerit, students[i]->Y_N); 
+        
+    }
+    printf("-------------------------------------------------------------\n\n");
+}
+
+
+//3. 벌점 부과하기
+void penalty_points(){
+
+
+}
+
+//4. 리스트 저장
+void save_list(){
+
+}
+
+//"5. 리스트 불러오기
+void recall_list(){
+
+}
--- manager.h
+++ manager.h
@@ -1,2 +1,7 @@
 //헤터파일.
 void manager_main();
+void room_numbering();
+void dormitory_list();
+void penalty_points();
+void save_list();
+void recall_list();
