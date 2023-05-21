54d5b907bd39436d6f58a4adeb5bc39d654658b5
SungyulLim <130723460+SungyulLim@users.noreply.github.com>
2023-05-21 17:13:23 +0900

    Update student.c
    1. checkMy_Info에서 오류 나는 부분 수정하기
    2. update_domitory에서 방 배정이 완료된 학생은 수정이 불가능 하도록 수정
    3. delete_domitory에서 방 배정이 완료된 학생은 삭제가 불가능 하도록 수정

--- student.c
+++ student.c
@@ -13,8 +13,6 @@
 // count 변수는 main.c 에서 0으로 초기화 했으니 사용 하시면 됩니다!
 
 void student_main() {
-  printf("학생용 서브메인 입니다\n");
-  
   while (1) {
     int menu = 0;
     printf("\n---학생용 기숙사 신청 시스템---\n");
@@ -57,7 +55,7 @@ void student_main() {
 void checkMyInfo() {
   char id[20];   // 직접 입력하게 될 ID
   char name[20]; // 직접 입력하게 될 Name
-  int k = 0;     // 학생이 몇번인지 확인하기 위한 변수
+  int k = 100;     // 학생이 몇번인지 확인하기 위한 변수
   printf("학번을 입력하시오. "); //학번 입력
   scanf("%s", id);
 
@@ -68,26 +66,32 @@ void checkMyInfo() {
     }
   }
 
-  printf("이름을 입력하시오. "); // 이름을 입력받기
-  scanf("%s", name);
+  if(k==100){
+    printf("입력하신 학번이 존재하지 않습니다. \n");
+  }
 
-  if (strcmp(name, students[k]->name) == 0) { //입력받은 이름과 학번이 일치하면 본인의 개인정보 공개
-    printf("********************\n");
-    printf("이름 : %s\n", students[k]->name);
-    printf("학번 : %s\n", students[k]->s_ID);
-    printf("성별 : %s\n", students[k]->f_m);
-    printf("기숙사 : %s\n", students[k]->Dormitory_name);
-    printf("인실 : %d\n", students[k]->total_num_room);
-    if(students[k]->Y_N == 'N'){
-      printf("호실 : 아직 방 배정이 되지 않았습니다.\n");
+  else{
+    printf("이름을 입력하시오. "); // 이름을 입력받기
+    scanf("%s", name);
+
+    if (strcmp(name, students[k]->name) == 0) { //입력받은 이름과 학번이 일치하면 본인의 개인정보 공개
+      printf("********************\n");
+      printf("이름 : %s\n", students[k]->name);
+      printf("학번 : %s\n", students[k]->s_ID);
+      printf("성별 : %s\n", students[k]->f_m);
+      printf("기숙사 : %s\n", students[k]->Dormitory_name);
+      printf("인실 : %d\n", students[k]->total_num_room);
+      if(students[k]->Y_N == 'N'){
+        printf("호실 : 아직 방 배정이 되지 않았습니다.\n");
+      }
+      else{
+        printf("호실 : %d\n", students[k]->room_num);
+      }
+      printf("벌점 : %d\n", students[k]->demerit);
+      printf("********************\n");
+    } else { //이름과 학번이 일치하지 않는 경우
+      printf("정보가 존재하지 않습니다. 다시 입력해주세요.\n");
     }
-    else{
-      printf("호실 : %d\n", students[k]->room_num);
-    }
-    printf("벌점 : %d\n", students[k]->demerit);
-    printf("********************\n");
-  } else { //이름과 학번이 일치하지 않는 경우
-    printf("정보가 존재하지 않습니다. 다시 입력해주세요.\n");
   }
 }
 
@@ -107,7 +111,7 @@ void apply_domitory() {
   students[count]->demerit = 0; //======> 이부분 추가
   students[count]->Y_N = 'N'; //======> 이부분 추가
 
-  printf("신청 성공!");
+  printf("\n신청 성공!\n");
   count++;
 }
 
@@ -129,26 +133,32 @@ void update_domitory() {
   printf("이름을 입력하시오. (처음 입력했던대로 입력하시오) "); // 이름을 입력받기
   scanf("%s", name);
 
-  if (strcmp(name, students[k]->name) == 0) {
-    printf("이름을 입력하시오. ");
-    scanf("%s", students[k]->name);
-    printf("학번을 입력하시오. ");
-    scanf("%s", students[k]->s_ID);
-    printf("성별을 입력하시오. ");
-    scanf("%s", students[k]->f_m);
-    printf("건물 이름을 입력하시오. (건물의 풀 네임으로 입력해주세요. ex) 로뎀관, 은혜관.) ");
-    scanf("%s", students[k]->Dormitory_name);
-    printf("몇인실을 신청하실 건가요? (창조관은 1인실, 갈대상자관은 1인실/2인실, 나머지 호관은 4인실/2인실이 신청 가능합니다.) ");
-    scanf("%d", &students[k]->total_num_room);
+  if(students[k]->Y_N=='Y'){
+    printf("기숙사 신청이 완료되어 삭제가 불가능합니다.\n");
   }
-  printf("수정 성공!");
+
+  else{
+    if (strcmp(name, students[k]->name) == 0) {
+      printf("이름을 입력하시오. ");
+      scanf("%s", students[k]->name);
+      printf("학번을 입력하시오. ");
+      scanf("%s", students[k]->s_ID);
+      printf("성별을 입력하시오. ");
+      scanf("%s", students[k]->f_m);
+      printf("건물 이름을 입력하시오. (건물의 풀 네임으로 입력해주세요. ex) 로뎀관, 은혜관.) ");
+      scanf("%s", students[k]->Dormitory_name);
+      printf("몇인실을 신청하실 건가요? (창조관은 1인실, 갈대상자관은 1인실/2인실, 나머지 호관은 4인실/2인실이 신청 가능합니다.) ");
+      scanf("%d", &students[k]->total_num_room);
+    }
+    printf("수정 성공!\n");
+  }
 }
 
 void delete_domitory() {
   char id[20];   // 직접 입력하게 될 ID
   char name[20]; // 직접 입력하게 될 Name
   int num = 0;
-  int k = 0; // 학생이 몇번인지 확인하기 위한 변수
+  int k = 100; // 학생이 몇번인지 확인하기 위한 변수
   printf("학번을 입력하시오. (처음 입력했던대로 입력하시오) "); //학번 입력
   scanf("%s", id);
 
@@ -160,32 +170,69 @@ void delete_domitory() {
     }
   }
 
+  if(k==100){
+    printf("입력하신 학번이 존재하지 않습니다. \n");
+  }
+
   printf("이름을 입력하시오. (처음 입력했던대로 입력하시오) "); // 이름을 입력받기
   scanf("%s", name);
 
-  if (strcmp(name, students[k]->name) == 0) {
-    printf("정말로 삭제하시겠습니까?(삭제 :1) ");
-    scanf("%d", &num);
-    if (num == 1) {
-      char a[20] = "\0";
-      strcpy(students[k]->name, a);
-      strcpy(students[k]->s_ID, a);
-      strcpy(students[k]->f_m, a);
-      strcpy(students[k]->Dormitory_name, a);
-      students[k]->total_num_room = -1;
-      printf("삭제 성공! ");
-    }
+  if(students[k]->Y_N=='Y'){
+    printf("기숙사 신청이 완료되어 삭제가 불가능합니다.\n");
   }
+
+  else{
+    if (strcmp(name, students[k]->name) == 0) {
+      printf("정말로 삭제하시겠습니까?(삭제 :1) ");
+      scanf("%d", &num);
+      if (num == 1) {
+        char a[20] = "\0";
+        strcpy(students[k]->name, a);
+        strcpy(students[k]->s_ID, a);
+        strcpy(students[k]->f_m, a);
+        strcpy(students[k]->Dormitory_name, a);
+        students[k]->total_num_room = -1;
+        printf("삭제 성공!\n");
+      }
+    }
+    }
 }
 
 void apply_overnight(){
-  printf("외박을 신청하실 날짜를 입력하시오. ");
-  getchar();
-  fgets(overnight-> overnight_date,10,stdin);
+  char id[20];   // 직접 입력하게 될 ID
+  char name[20]; // 직접 입력하게 될 Name
+  int num = 0;
+  int k = 100; // 학생이 몇번인지 확인하기 위한 변수
+  printf("학번을 입력하시오. (처음 입력했던대로 입력하시오) "); //학번 입력
+  scanf("%s", id);
 
-  printf("외박신청사유를 입력하시오. ");
-  getchar();
-  fgets(overnight->overnight_reason,50,stdin);
+  for (int i = 0; i < count; i++) {
+    if (strcmp(id, students[i]->s_ID) ==
+        0) { // 입력한 학번과 일치하는 학번이 있는지 확인
+      k = i;
+      break;
+    }
+  }
+  
+  if(k==100){
+    printf("입력하신 학번이 존재하지 않습니다. \n");
+  }
 
-  printf("외박 신청이 완료되었습니다! \n");
+  else{
+    printf("이름을 입력하시오. (처음 입력했던대로 입력하시오) "); // 이름을 입력받기
+    scanf("%s", name);
+    
+    if(strcmp(name, students[k]->name) == 0){
+      printf("외박을 신청하실 날짜를 입력하시오. ( 예시: 5.18 ) ");
+      scanf("%s", overnight[k]-> overnight_date);
+
+      printf("외박신청사유를 입력하시오. ");
+      scanf("%s", overnight[k]-> overnight_reason);
+
+      printf("외박 신청이 완료되었습니다! \n");
+    }
+    else{
+      printf("이름이 잘못 입력되었습니다.\n");
+    }
+  }
 }
